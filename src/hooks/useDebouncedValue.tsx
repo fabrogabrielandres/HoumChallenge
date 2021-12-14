c5df984cf811793e758dfc47330d7f4b30062be7
Gabriel Fabro <fabrogabrielandres@gmail.com>
2021-12-14 16:06:39 -0600

    Implement filter

--- src/hooks/useDebouncedValue.tsx
+++ src/hooks/useDebouncedValue.tsx
@@ -1,22 +1,21 @@
-import  { useState } from 'react'
+import { useState } from 'react';
 import { useEffect } from 'react';
 
-export const useDebouncedValue = ( input: string = "" , time: number = 500 ) => {
-    
-    const [debounceValue, setDebounceValue] = useState(input)
+export const useDebouncedValue = (input: string = '', time: number = 500) => {
+	const [ debounceValue, setDebounceValue ] = useState(input);
 
-    useEffect(() => {
-        const timeOut = setTimeout(() => {
-            setDebounceValue(input)            
-        }, time); 
+	useEffect(
+		() => {
+			const timeOut = setTimeout(() => {
+				setDebounceValue(input);
+			}, time);
+			return () => {
+				clearTimeout(timeOut);
+			};
+		},
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+		[ input ]
+	);
 
-        return () => {
-            clearTimeout(timeOut)
-        }
-    }, [input])
-
-
-    return (
-        debounceValue        
-    )
-}
+	return { debounceValue };
+};
--- src/screen/SearchScreen.tsx
+++ src/screen/SearchScreen.tsx
@@ -1,38 +1,61 @@
 import { Flex, Text, InputGroup, InputLeftElement, Input, Box, Img } from '@chakra-ui/react';
 import { Search2Icon } from '@chakra-ui/icons';
 import { usePokemonSearch } from '../hooks/usePokemonSearch';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Loader } from '../components/Loader/Loader';
 import { CardPokemon } from '../components/CardPokemon/CardPokemon';
+import { useDebouncedValue } from '../hooks/useDebouncedValue';
+import { Pokemon } from '../interfaces/interface';
 
 export const SearchScreen = () => {
 	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	const [ query, setQuery ] = useState('');
 	const { pokemonList, loadPokemons, isFetching } = usePokemonSearch();
+	const [ pokemonFiltered, setPokemonFiltered ] = useState<Pokemon[]>([]);
+
+	const { debounceValue } = useDebouncedValue(query, 500);
 
 	useEffect(() => {
 		loadPokemons();
 	}, []);
 
+	useEffect(
+		() => {
+			if (debounceValue.length === 0) {
+				return setPokemonFiltered([]);
+			}
+			setPokemonFiltered(
+				pokemonList.filter((pokemon) =>
+					pokemon.name.toLocaleLowerCase().includes(debounceValue.toLocaleLowerCase())
+				)
+			);
+		},
+		[ debounceValue ]
+	);
+
 	return isFetching ? (
 		<Loader />
 	) : (
-		<Flex flexWrap="wrap" justifyContent="center">
+		<>
+				<Flex flexWrap="wrap" justifyContent="center">
 			<Text mt={10} display="inline" fontSize="x-large" color="#ff452b">
 				Search Pokemon by Id or Name...
 			</Text>
 			<Box w={'100%'} mt={10}>
 				<InputGroup w="70%" margin="0 auto" boxShadow="dark-lg">
 					<InputLeftElement pointerEvents="none" children={<Search2Icon color="gray.300" />} />
-					<Input type="tel" placeholder="Phone number" />
+					<Input type="tel" placeholder="Phone number" onChange={(e) => setQuery(e.target.value)} />
 				</InputGroup>
 			</Box>
-			<Flex justifyContent={"space-evenly"} flexWrap={"wrap"} >
-				{pokemonList.map((pokemon, idx) => {
-					return (
-						<Img src={pokemon.imageUrl}/>
-					)
+			<Text mt={10} display="block" fontSize="x-large" color="#ff452b">
+				{`Pokemon: ${debounceValue}`}
+			</Text>
+				</Flex>
+			<Flex justifyContent={'space-evenly'} flexWrap={'wrap'}>
+				{pokemonFiltered.map((pokemon, idx) => {
+					return <CardPokemon pokemon={pokemon} key={idx} />;
 				})}
 			</Flex>
-		</Flex>
+				</>
 	);
 };
