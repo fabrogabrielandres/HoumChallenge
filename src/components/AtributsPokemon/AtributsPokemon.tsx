660ba10877a7b55d26d1851cd8f563fa12cdd662
Gabriel Fabro <fabrogabrielandres@gmail.com>
2021-12-14 21:49:34 -0600

    fixed card

--- src/components/AtributsPokemon/AtributsPokemon.tsx
+++ src/components/AtributsPokemon/AtributsPokemon.tsx
@@ -12,7 +12,6 @@ interface Props {
 
 export const AtributsPokemon = ({ atributs, id }: Props) => {
 	const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`;
-	console.log(atributs.name);
 
 	return (
 		<Box>
--- src/components/CardPokemon/CardPokemon.tsx
+++ src/components/CardPokemon/CardPokemon.tsx
@@ -1,18 +1,21 @@
 import React, { useState } from 'react';
-import { Img, Text, Box } from '@chakra-ui/react';
 import { Pokemon } from '../../interfaces/interface';
 import { useNavigate } from 'react-router-dom';
-import './styles.css';
 import { ColorExtractor } from 'react-color-extractor';
 import pokebola from '../../assets/pokebola.png';
+import { Img } from '@chakra-ui/react';
+import { Center, Box, Avatar, Stack, Heading, Text, useColorModeValue, Badge } from '@chakra-ui/react';
+import './styles.css';
+
 interface Props {
 	pokemon: Pokemon;
 }
+
 export const CardPokemon = ({ pokemon }: Props) => {
 	const [ palettColors, setPalettColors ] = useState([]);
 	const { id, imageUrl, name } = pokemon;
 	let navigate = useNavigate();
-	
+	let sprite = [ '', 'back', 'back/shiny', 'shiny' ];
 
 
 	const getColors = (colors: []) => {
@@ -20,38 +23,74 @@ export const CardPokemon = ({ pokemon }: Props) => {
 	};
 
 	return (
-		<Box
-			className="container"
-			boxShadow="2xl"
-			mt={50}
-			transition={'all 0.2s ease-in-out'}
-			bgGradient={`linear(to-r,${palettColors[0]} ,${palettColors[1]})`}
-			_hover={{
-				bgGradient: `linear(to-l,${palettColors[4]},${palettColors[2]})`,
-				transform: 'scale(1.09)'
-			}}
-		>
-			<Text fontSize="2em" mt={'50%'} textAlign={'center'} color={'#ff452b'} textTransform={'uppercase'}>
-				{name}
-			</Text>
-			<Text fontSize="1.5em" textAlign={'center'} color={'#ff452b'}>
-				#{id}
-			</Text>
+		<Center py={6} mt={20} mr={10}>
 			<Box
-				position={'absolute'}
-				w={'20%'}
-				left={0}
-				bottom={0}
-				onClick={() => {
-					navigate(`/pokemon/${id}`);
+				maxW={'320px'}
+				w={'full'}
+				bgGradient={`linear(to-r,${palettColors[0]} ,${palettColors[1]})`}
+				_hover={{
+					bgGradient: `linear(to-l,${palettColors[4]},${palettColors[2]})`,
+					transform: 'scale(1.09)'
 				}}
+				boxShadow={'2xl'}
+				rounded={'lg'}
+				p={6}
+				textAlign={'center'}
 			>
-				<Img src={pokebola} objectFit={'cover'} bg={'white'} cursor={'pointer'} className="efecto" />
-			</Box>
-			<Box position={'absolute'} w={'70%'} right={0} top={'-15%'}>
-				<Img src={imageUrl} objectFit={'cover'} />
+				<Box position={'relative'}>
+					<Avatar
+						top={-20}
+						right={-10}
+						size={'xl'}
+						src={imageUrl}
+						alt={'Avatar Alt'}
+						mb={4}
+						position={'absolute'}
+						_after={{
+							content: '""',
+							w: 4,
+							h: 4,
+							bg: 'green.300',
+							border: '2px solid white',
+							rounded: 'full',
+							pos: 'absolute',
+							bottom: 0,
+							right: 3
+						}}
+					/>
+				</Box>
+				<Heading fontSize={'2xl'} fontFamily={'body'} color="#ff452b">
+					{name.toUpperCase()}
+				</Heading>
+				<Text mt={10} fontWeight={600} color="#ff452b" mb={4}>
+					{`ID: #${id}`}
+				</Text>
+				<Stack align={'center'} justify={'center'} direction={'row'} mt={6}>
+					{sprite.map((item, idx) => {
+						return (
+							<Img
+								key={idx}
+								w={'25%'}
+								src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${item}/${id}.png`}
+							/>
+						);
+					})}
+				</Stack>
+
+				<Stack mt={10} direction={'row'} spacing={4} justifyContent="center">
+					<Img
+						src={pokebola}
+						w={'20%'}
+						objectFit={'cover'}
+						className="efecto"
+						onClick={() => {
+							navigate(`/pokemon/${id}`);
+						}}
+						cursor="pointer"
+					/>
+				</Stack>
 			</Box>
 			<ColorExtractor src={imageUrl} getColors={(colors: any) => getColors(colors)} />
-		</Box>
+		</Center>
 	);
 };
